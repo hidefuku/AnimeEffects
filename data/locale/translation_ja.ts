36c2acd3530ce9c8ce867d1182df061f99554802
hidefuku <fukkun1-11@hotmail.co.jp>
2016-11-24 01:04:13 +0900

    Opps! This time for sure, I added translation file.

---
+++ data/locale/translation_ja.ts
@@ -0,0 +1,61 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja_JP">
+<context>
+    <name>gui::MainMenuBar</name>
+    <message>
+        <location filename="../../src/gui/Main.cpp" line="534"/>
+        <location filename="../../src/gui/MainMenuBar.cpp" line="25"/>
+        <source>File</source>
+        <translation>ファイル</translation>
+    </message>
+    <message>
+        <location filename="../../src/gui/MainMenuBar.cpp" line="69"/>
+        <source>Edit</source>
+        <translation>編集</translation>
+    </message>
+    <message>
+        <location filename="../../src/gui/MainMenuBar.cpp" line="84"/>
+        <source>Project</source>
+        <translation>プロジェクト</translation>
+    </message>
+    <message>
+        <location filename="../../src/gui/MainMenuBar.cpp" line="103"/>
+        <source>Window</source>
+        <translation>ウィンドウ</translation>
+    </message>
+    <message>
+        <location filename="../../src/gui/MainMenuBar.cpp" line="127"/>
+        <source>Option</source>
+        <translation>オプション</translation>
+    </message>
+    <message>
+        <location filename="../../src/gui/MainMenuBar.cpp" line="142"/>
+        <source>Help</source>
+        <translation>ヘルプ</translation>
+    </message>
+</context>
+<context>
+    <name>gui::MainWindow</name>
+    <message>
+        <location filename="../../src/gui/MainWindow.cpp" line="129"/>
+        <source>Target Dock</source>
+        <translation>ターゲット</translation>
+    </message>
+    <message>
+        <location filename="../../src/gui/MainWindow.cpp" line="141"/>
+        <source>Property Dock</source>
+        <translation>プロパティ</translation>
+    </message>
+    <message>
+        <location filename="../../src/gui/MainWindow.cpp" line="182"/>
+        <source>Tool Dock</source>
+        <translation>ツール</translation>
+    </message>
+    <message>
+        <location filename="../../src/gui/MainWindow.cpp" line="635"/>
+        <source>Exporting File</source>
+        <translation>出力ファイル</translation>
+    </message>
+</context>
+</TS>
